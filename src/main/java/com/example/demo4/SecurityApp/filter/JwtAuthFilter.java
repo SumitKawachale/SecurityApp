72a8bac14971d1f548809df164a085dc0aa592bf
Sumit Kawachale <sumit.study.1603@gmail.com>
2026-05-19 23:22:11 +0530

    feat(Security): implement JWT authentication filter

---
+++ src/main/java/com/example/demo4/SecurityApp/filter/JwtAuthFilter.java
@@ -0,0 +1,63 @@
+package com.example.demo4.SecurityApp.filter;
+
+import com.example.demo4.SecurityApp.dto.LoginDTO;
+import com.example.demo4.SecurityApp.entities.User;
+import com.example.demo4.SecurityApp.services.JwtService;
+import com.example.demo4.SecurityApp.services.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserService userService;
+
+    @Qualifier("handlerExceptionResolver")
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        try{
+            final String requestTokenHeader = request.getHeader("Authorization");
+            if(requestTokenHeader == null || !requestTokenHeader.startsWith("Bearer")){
+                filterChain.doFilter(request,response);
+                return;
+            }
+
+            String token = requestTokenHeader.substring(7).trim();
+            Long userId = jwtService.getUserIdFromToken(token);
+
+            if(userId != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                User user = userService.getUserById(userId);
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(user, null,user.getAuthorities());
+                authenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+            filterChain.doFilter(request,response);
+        }catch (Exception ex){
+            handlerExceptionResolver.resolveException(request,response,null,ex);
+        }
+    }
+}
